cfec00cccf49cdddbd186436103694b1ef4db66d
Thiago <thiagojapa58@gmail.com>
2022-05-14 22:01:51 -0300

    criado rota padrão e readme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response} from 'express';
 import cors from 'cors';
+import { request } from 'http';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.get('/', (request: Request, response: Response) => {
+    return response.send('OK')
+})
 
 app.listen(8080, () => {
     console.log('API rodando');  
